6e5c1a581f0110c2935441ea38fc3f4c62d0573d
jfbyers <jfbyers@users.noreply.github.com>
2015-05-07 12:40:08 +0200

    Create network-monitor.sh

---
+++ network-monitor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ 
+LOGFILE=/tmp/network-monitor.log
+ 
+ 
+# Which Interface do you want to check
+wlan='wlan0'
+# Which address do you want to ping to see if you can connect.
+pingip='google.com'
+ 
+# Perform the network check and reset if necessary
+/bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
+if [ $? -ge 1 ] ; then
+  echo "$(date "+%m %d %Y %T") : WIFI connection down! Attempting reconnection." >> $LOGFILE
+  /sbin/ifdown $wlan
+  sleep 10
+  /sbin/ifup --force $wlan
+  OUT=$? #save exit status of last command to decide what to do next
+  sleep 10
+  if [ $OUT -eq 0 ] ; then
+    STATE=$(ifconfig wlan0 | grep "inet addr:")
+    EXTERNAL_IP=$(/usr/bin/curl ifconfig.me/ip)
+    echo "$(date "+%m %d %Y %T") : Network connection reset. Current state is $STATE , internet address is $EXTERNAL_IP">> $LOGFILE
+  else
+    echo "$(date "+%m %d %Y %T") : Failed to reset wifi connection" >> $LOGFILE
+  fi
+else
+   EXTERNAL_IP=$(/usr/bin/curl ifconfig.me/ip)
+   echo "$(date "+%m %d %Y %T") : WIFI OK, internet address is $EXTERNAL_IP" >> $LOGFILE
+fi
+ 
+exit 0
